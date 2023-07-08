2848b7c602738017716ec8c9b850e5d0d3885753
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-07-08 16:16:29 +0100

    Added some extra parameters to display the description and hide the show missing fields widget.

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -6,31 +6,45 @@ import EventContext from '../context/EventContext'
 import { RenderDate, venueFactory } from './readMore/ReadMore'
 import useTimeFormat from '../hooks/useTimeFormat'
 import useLanguage from '../hooks/useLanguage'
+import { extractParameter } from '../utils/paramExtraction'
 
 function EventDisplay ({ currentEvent }) {
-    const timeFormat = useTimeFormat()
-    useLanguage()
-    if (!!currentEvent) {
-        const date = currentEvent.dateList && currentEvent.dateList.length > 0
-            ? currentEvent.dateList[0] : null
-        if (!!date) {
-            return (
-                <>
-                    <h2>{currentEvent.name}</h2>
-                    {!!currentEvent.subTitle && <h3>{currentEvent.subTitle}</h3>}
-                    <div className="row mb-3 mt-3">
-                        <div className="col-12">
-                            <RenderDate date={date}
-                                        currentEvent={venueFactory(
-                                            currentEvent)}
-                                        timeFormat={timeFormat}/>
-                        </div>
-                    </div>
-                </>
-            )
-        }
+  const timeFormat = useTimeFormat()
+
+  const hideEventDate = extractParameter(null,
+    'hideEventDate', false)
+  const showShortDescription = extractParameter(null,
+    'showShortDescription', false)
+  const showDescription = extractParameter(null,
+    'showDescription', false)
+
+  useLanguage()
+  if (!!currentEvent) {
+    const date = currentEvent.dateList && currentEvent.dateList.length > 0
+      ? currentEvent.dateList[0] : null
+    if (!!date) {
+      return (
+        <>
+          <h2>{currentEvent.name}</h2>
+          {!!currentEvent.subTitle && <h3>{currentEvent.subTitle}</h3>}
+          {showShortDescription && <div className="row mb-3 mt-3">
+            <div className="col-12">
+              {currentEvent.shortDescription}
+            </div>
+          </div>}
+          {!hideEventDate && <div className="row mb-3 mt-3">
+            <div className="col-12">
+              <RenderDate date={date}
+                          currentEvent={venueFactory(
+                            currentEvent)}
+                          timeFormat={timeFormat}/>
+            </div>
+          </div>}
+        </>
+      )
     }
-    return <></>
+  }
+  return <></>
 }
 
 /**
@@ -41,15 +55,15 @@ function EventDisplay ({ currentEvent }) {
  */
 function EventForm (props) {
 
-    const { currentEvent } = useContext(EventContext)
+  const { currentEvent } = useContext(EventContext)
 
-    return (
-        <DetailRenderer origProps={{ ...props }}>
-            <EventDisplay currentEvent={currentEvent}/>
-            <IncludeForm currentEvent={currentEvent}
-                         className="col-12 pl-1 pr-1"/>
-        </DetailRenderer>
-    )
+  return (
+    <DetailRenderer origProps={{ ...props }}>
+      <EventDisplay currentEvent={currentEvent}/>
+      <IncludeForm currentEvent={currentEvent}
+                   className="col-12 pl-1 pr-1"/>
+    </DetailRenderer>
+  )
 }
 
 export default withRouter(EventForm)
