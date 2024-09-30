59bfb1440d2ac440291c1d17e4ab513038a94afa
Gil Fernandes <gil.fernandes@onepointltd.com>
2024-09-30 17:57:00 +0100

    Added more explicit styles for mandatory fields.

--- src/components/formio/consent/ConsentRenderer.jsx
+++ src/components/formio/consent/ConsentRenderer.jsx
@@ -5,7 +5,23 @@ import React from "react";
  * Renders a checkbox (defaults to false)
  */
 export default class ConsentRenderer extends CheckboxRenderer {
+
   constructor(props) {
     super(props);
+  }
+
+
+  render() {
+    return (
+      <>
+        <input
+          type="checkbox"
+          checked={this.state.value}
+          id={this.props.component.id}
+          onChange={this.setValue}
+          required={this?.props?.component?.validate?.required}
+        />
+      </>
+    );
   }
 }
